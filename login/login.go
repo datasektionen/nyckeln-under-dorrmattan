19401dc3cc80428506820bca9c702e375f06b8d4
Mathias Magnusson <mathias@magnusson.space>
2023-09-28 21:58:09 +0200

    Allow setting a different initial kth id

--- login/login.go
+++ login/login.go
@@ -5,15 +5,18 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
 	"strings"
 )
 
 var port int
 var hodisURL string
+var initKTHID string
 
 func init() {
 	flag.IntVar(&port, "login-port", 7002, "port to listen on for login")
 	flag.StringVar(&hodisURL, "hodis-url", "https://hodis.datasektionen.se", "url to hodis")
+	flag.StringVar(&initKTHID, "kth-id", os.Getenv("KTH_ID"), "kth id to login as. can be overwritten by writing on stdin")
 }
 
 type loginUser struct {
@@ -38,6 +41,15 @@ func Listen(kthIDs <-chan string) {
 		UGKthid:   "u1jwkms6",
 	}
 
+	if initKTHID != "" {
+		if u, err := getUserFromHodis(initKTHID); err != nil {
+			fmt.Println(err)
+		} else {
+			user = u
+		}
+	}
+	fmt.Println("Now logging in as:", user.FirstName, user.LastName)
+
 	h.HandleFunc("/hello", func(w http.ResponseWriter, _ *http.Request) {
 		w.Write([]byte("Hello Login!!!!"))
 	})
@@ -58,37 +70,45 @@ func Listen(kthIDs <-chan string) {
 		}
 	}()
 	for kthID := range kthIDs {
-		res, err := http.Get(hodisURL + "/users/" + kthID)
+		var err error
+		user, err = getUserFromHodis(kthID)
 		if err != nil {
 			fmt.Println(err)
+			fmt.Println("Still logging in as:", user.FirstName, user.LastName)
 			continue
 		}
-		var users []struct {
-			UGKthid     string `json:"ugKthid"`
-			UID         string `json:"uid"`
-			CN          string `json:"cn"`
-			Mail        string `json:"mail"`
-			GivenName   string `json:"givenName"`
-			DisplayName string `json:"displayName"`
-			Year        int    `json:"year"`
-			Tag         string `json:"tag"`
-		}
-		json.NewDecoder(res.Body).Decode(&users)
-		if len(users) == 0 {
-			fmt.Println("no users found")
-			continue
-		}
-		hodis := users[0]
-		user = loginUser{
-			FirstName: hodis.GivenName,
-			LastName: strings.TrimPrefix(
-				hodis.DisplayName,
-				hodis.GivenName+" ",
-			),
-			User:    hodis.UID,
-			Email:   hodis.Mail,
-			UGKthid: hodis.UGKthid,
-		}
-		fmt.Println("Now logging in as:", hodis.CN)
+		fmt.Println("Now logging in as:", user.FirstName, user.LastName)
 	}
+}
+
+func getUserFromHodis(kthID string) (loginUser, error) {
+	res, err := http.Get(hodisURL + "/users/" + kthID)
+	if err != nil {
+		return loginUser{}, nil
+	}
+	var users []struct {
+		UGKthid     string `json:"ugKthid"`
+		UID         string `json:"uid"`
+		CN          string `json:"cn"`
+		Mail        string `json:"mail"`
+		GivenName   string `json:"givenName"`
+		DisplayName string `json:"displayName"`
+		Year        int    `json:"year"`
+		Tag         string `json:"tag"`
+	}
+	json.NewDecoder(res.Body).Decode(&users)
+	if len(users) == 0 {
+		return loginUser{}, fmt.Errorf("No user with the kth id '%s' found", kthID)
+	}
+	hodis := users[0]
+	return loginUser{
+		FirstName: hodis.GivenName,
+		LastName: strings.TrimPrefix(
+			hodis.DisplayName,
+			hodis.GivenName+" ",
+		),
+		User:    hodis.UID,
+		Email:   hodis.Mail,
+		UGKthid: hodis.UGKthid,
+	}, nil
 }
